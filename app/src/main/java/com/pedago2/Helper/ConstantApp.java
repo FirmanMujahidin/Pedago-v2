0a3189efb63adf8e0f2e77bca48211a0f0f9c3ed
Firman mujahidin <firmanmujahidin@gmail.com>
2017-10-26 15:44:46 +0700

    add code apiService

---
+++ app/src/main/java/com/pedago2/Helper/ConstantApp.java
@@ -0,0 +1,12 @@
+package com.pedago2.Helper;
+
+/**
+ * Created by firma on 26-Oct-17.
+ */
+
+public class ConstantApp {
+    //api auth
+    public static final String API_CODE = "pedagoStudent";
+    public static final String API_AUTH = "api_username";
+    public static final String API_KEY = "api_key";
+}
--- app/src/main/java/com/pedago2/Service/ApiClient.java
+++ app/src/main/java/com/pedago2/Service/ApiClient.java
@@ -5,4 +5,67 @@ package com.pedago2.Service;
  */
 
 public class ApiClient {
+	
+	 private static final String TAG = "ApiClient";
+
+    public static Retrofit request(OnRequestTimeOut listener) {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .addInterceptor(chain -> {
+                    Request original = chain.request();
+                    HttpUrl originalHttpUrl = original.url();
+                    HttpUrl url = null;
+
+                    HttpUrl.Builder builder = originalHttpUrl.newBuilder();
+
+                    if (original.header("no_param") == null) {
+                        builder.addQueryParameter("api_code", API_CODE);
+                        builder.addQueryParameter("tipe_output", original.header("type_output"));
+                        if (original.header("type_output").equals("6")) {
+                            if (original.header("limit") != null)
+                                builder.addQueryParameter("limit", original.header("limit"));
+                            else
+                                builder.addQueryParameter("limit", "0");
+                        } else
+                            builder.addQueryParameter("api_proses", "1");
+
+                        if (original.header("No-Auth") != null) {
+                            builder.addQueryParameter("get_user", "1");
+                        } else {
+                        }
+                    }
+
+                    url = builder.build();
+
+                    Request.Builder requestBuilder = original.newBuilder()
+                            .url(url);
+
+                    Request request = requestBuilder.build();
+
+                    try {
+                        return chain.proceed(request);
+                    } catch (SocketTimeoutException exception) {
+                        exception.printStackTrace();
+                    }
+
+                    return chain.proceed(request);
+                })
+
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .build();
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        return new Retrofit.Builder()
+                .baseUrl("https://pedago.id/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+            }
 }
